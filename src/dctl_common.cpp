adafb40e2951a46aaec834f970c4309ca1f510fe
Andrey Basov <dev.basov@gmail.com>
2022-01-02 19:27:48 +0300

    collision checks and NextState

--- src/dctl_common.cpp
+++ src/dctl_common.cpp
@@ -30,6 +30,28 @@ Vector2 NewPos(Dir dir, float dist, Vector2 cur_pos) {
   return cur_pos;
 }
 
+State NextState(const State &st, const std::unordered_map<int, Input> &inp,
+                double dt, float speed, size_t max_length) {
+  State ns;
+  ns.sequence = st.sequence + 1;
+  for (auto &i : st.snakes) {
+    auto tmp = i;
+    if (inp.find(i.player_id) != inp.end()) {
+      auto newDir = NewDir(i.dir, inp.at(i.player_id));
+      if (newDir != i.dir) {
+        tmp.dir = newDir;
+        tmp.tail.push_back(tmp.tail.back());
+        if (tmp.tail.size() > max_length) {
+          tmp.tail.pop_front();
+        }
+      }
+    }
+    tmp.tail.back() = NewPos(tmp.dir, dt * speed, tmp.tail.back());
+    ns.snakes.push_back(tmp);
+  }
+  return ns;
+}
+
 // https://stackoverflow.com/a/14177062
 bool IsIntersecting(const Vector2 &p1, const Vector2 &p2, const Vector2 &q1,
                     const Vector2 &q2) {
@@ -39,19 +61,6 @@ bool IsIntersecting(const Vector2 &p1, const Vector2 &p2, const Vector2 &q1,
          (((p1.x - q1.x) * (q2.y - q1.y) - (p1.y - q1.y) * (q2.x - q1.x)) *
               ((p2.x - q1.x) * (q2.y - q1.y) - (p2.y - q1.y) * (q2.x - q1.x)) <
           0);
-}
-
-bool IsSelfIntersecting(const Snake &snake) {
-  if (snake.tail.size() > 4) {
-    auto head = snake.tail.rbegin();
-    auto neck = snake.tail.rbegin() + 1;
-    for (auto it = snake.tail.rbegin() + 2; it != snake.tail.rend(); it++) {
-      if (IsIntersecting(*head, *neck, *it, *(it - 1))) {
-        return true;
-      }
-    }
-  }
-  return false;
 }
 
 Vector2 IntersectionPoint(const Vector2 &p1, const Vector2 &p2,
@@ -80,7 +89,7 @@ bool IsCrossing(const Snake &snake1, const Snake &snake2) {
         Vector2Distance(*snake2.tail.rbegin(), p)) {
       return true;
     }
-  }else if (snake2.tail.size() > 2) {
+  } else if (snake2.tail.size() > 2) {
     for (auto it = snake2.tail.rbegin() + 2; it != snake2.tail.rend(); it++) {
       if (IsIntersecting(*head1, *neck1, *it, *(it - 1))) {
         return true;
@@ -90,14 +99,54 @@ bool IsCrossing(const Snake &snake1, const Snake &snake2) {
   return false;
 }
 
+State CheckCollisions(const State &st) {
+  State ns;
+  ns.sequence = st.sequence;
+  auto valid = GetPlayers(st);
+  for (auto &i : st.snakes) {
+    for (auto &j : st.snakes) {
+      if (IsCrossing(i, j)) {
+        valid.erase(i.player_id);
+      }
+    }
+  }
+  for (auto &i : st.snakes) {
+    if (valid.find(i.player_id) != valid.end()) {
+      ns.snakes.push_back(i);
+    }
+  }
+  return ns;
+}
+
+State CheckBounds(const State &st, float map_width, float map_height) {
+  State ns;
+  ns.sequence = st.sequence;
+  for (auto &i : st.snakes) {
+    if ((i.tail.back().x > 0) && (i.tail.back().x < map_width) &&
+        (i.tail.back().y > 0) && (i.tail.back().y < map_height)) {
+      ns.snakes.push_back(i);
+    }
+  }
+  return ns;
+}
+
+std::unordered_set<int> GetPlayers(const State &st) {
+  std::unordered_set<int> result;
+  for (auto &i : st.snakes) {
+    result.insert(i.player_id);
+  }
+  return result;
+}
+
 void DrawLineExRoundEnd(Vector2 start_pos, Vector2 end_pos, float thick,
-                              Color color) {
+                        Color color) {
   DrawLineEx(start_pos, end_pos, thick, color);
   DrawCircleV(start_pos, thick / 2, color);
   DrawCircleV(end_pos, thick / 2, color);
 }
 
-void Draw(const State &st, const float map_width, const float map_height, const float scale) {
+void Draw(const State &st, const float map_width, const float map_height,
+          const float scale) {
   ClearBackground(BLACK);
   for (auto i = 0; i < map_width; i++) {
     for (auto j = 0; j < map_height; j++) {
@@ -114,7 +163,7 @@ void Draw(const State &st, const float map_width, const float map_height, const 
           {(it + 1)->x * (float)scale, (it + 1)->y * (float)scale}, thick,
           curColor);
     }
-    DrawCircle(curTail.back().x * (float)scale,
-               curTail.back().y * (float)scale, (float)scale / 2, curColor);
+    DrawCircle(curTail.back().x * (float)scale, curTail.back().y * (float)scale,
+               (float)scale / 2, curColor);
   }
 }
--- src/dctl_common.h
+++ src/dctl_common.h
@@ -4,6 +4,7 @@
 
 #include <deque>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include "raylib.h"
@@ -41,13 +42,21 @@ Dir NewDir(const Dir &cur_dir, const Input &inp);
 
 Vector2 NewPos(Dir dir, float dist, Vector2 cur_pos);
 
+State NextState(const State &st, const std::unordered_map<int, Input> &inp,
+                double dt, float speed, size_t max_length);
+
 bool IsIntersecting(const Vector2 &p1, const Vector2 &p2, const Vector2 &q1,
                     const Vector2 &q2);
 
-bool IsSelfIntersecting(const Snake &snake);
-
 bool IsCrossing(const Snake &snake1, const Snake &snake2);
 
+State CheckCollisions(const State &st);
+
+State CheckBounds(const State &st, float map_width, float map_height);
+
+std::unordered_set<int> GetPlayers(const State &st);
+
 void DrawLineExRoundEnd(Vector2 start_pos, Vector2 end_pos, float thick,
-                              Color color);
-void Draw(const State &st, const float map_width, const float map_height, const float scale);
+                        Color color);
+void Draw(const State &st, const float map_width, const float map_height,
+          const float scale);
