a0ec2e0a8fe61b2c2b4e84c1f00ece747dd7f08e
Andrey Basov <dev.basov@gmail.com>
2022-01-08 19:49:57 +0300

    DrawLineExSquareEnd function added

--- src/dctl_raylib.cpp
+++ src/dctl_raylib.cpp
@@ -7,6 +7,19 @@ void DrawLineExRoundEnd(Vec2 start_pos, Vec2 end_pos, float thick,
   DrawCircleV({end_pos.x, end_pos.y}, thick / 2, color);
 }
 
+void DrawLineExSquareEnd(Vec2 start_pos, Vec2 end_pos, float thick,
+                         Color color) {
+  Vector2 v1{start_pos.x, start_pos.y};
+  Vector2 v2{end_pos.x, end_pos.y};
+  Vector2 center = Vector2Lerp(v1, v2, 0.5);
+  auto len = Vector2Distance(v1,v2)+thick;
+  auto dir = Vector2Normalize(Vector2Subtract(v1,v2));
+  auto half = Vector2Scale(dir,len/2);
+  v1= Vector2Add(center, half);
+  v2= Vector2Subtract(center, half);
+  DrawLineEx(v1, v2, thick, color);
+}
+
 void Draw(const State &st, const float map_width, const float map_height,
           const float scale, const float head_diameter,
           const float tail_width) {
@@ -18,12 +31,11 @@ void Draw(const State &st, const float map_width, const float map_height,
   }
   for (auto &s : st.snakes) {
     auto &curTail = s.tail;
-    Color curColor{s.color.r,s.color.g,s.color.b,s.color.a};
+    Color curColor{s.color.r, s.color.g, s.color.b, s.color.a};
     float thick = tail_width * scale;
     for (auto it = curTail.begin(); it != curTail.end() - 1; it++) {
-      DrawLineExRoundEnd({it->x * scale, it->y * scale},
-                         {(it + 1)->x * scale, (it + 1)->y * scale}, thick,
-                         curColor);
+      DrawLineExSquareEnd({it->x * scale, it->y * scale},
+                 {(it + 1)->x * scale, (it + 1)->y * scale}, thick, curColor);
     }
     DrawCircle(curTail.back().x * scale, curTail.back().y * scale,
                (head_diameter / 2) * scale, curColor);
--- src/dctl_raylib.h
+++ src/dctl_raylib.h
@@ -7,5 +7,7 @@
 
 void DrawLineExRoundEnd(Vec2 start_pos, Vec2 end_pos, float thick, Color color);
 
+void DrawLineExSquareEnd(Vec2 start_pos, Vec2 end_pos, float thick, Color color);
+
 void Draw(const State &st, const float map_width, const float map_height,
           const float scale, const float head_diameter, const float tail_width);
