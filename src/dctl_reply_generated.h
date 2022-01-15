e77f5b5a30ad238af381fb07db9dce8b68b706ad
Andrey Basov <dev.basov@gmail.com>
2022-01-16 00:57:08 +0300

    reply flatbuffer message

--- src/dctl_reply_generated.h
+++ src/dctl_reply_generated.h
@@ -10,160 +10,166 @@ namespace dctl {
 namespace flat_settings {
 
 struct Settings;
-struct SettingsBuilder;
 
-struct Settings FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
-  typedef SettingsBuilder Builder;
-  enum FlatBuffersVTableOffset FLATBUFFERS_VTABLE_UNDERLYING_TYPE {
-    VT_MAX_CLIENTS = 4,
-    VT_MAP_WIDTH = 6,
-    VT_MAP_HEIGHT = 8,
-    VT_SPEED = 10,
-    VT_DT = 12,
-    VT_MAX_LENGTH = 14,
-    VT_HEAD_DIAMETER = 16,
-    VT_TAIL_WIDTH = 18
-  };
+struct Reply;
+struct ReplyBuilder;
+
+FLATBUFFERS_MANUALLY_ALIGNED_STRUCT(8) Settings FLATBUFFERS_FINAL_CLASS {
+ private:
+  uint32_t max_clients_;
+  float map_width_;
+  float map_height_;
+  float speed_;
+  double dt_;
+  uint32_t max_length_;
+  float head_diameter_;
+  float tail_width_;
+  int32_t padding0__;
+
+ public:
+  Settings()
+      : max_clients_(0),
+        map_width_(0),
+        map_height_(0),
+        speed_(0),
+        dt_(0),
+        max_length_(0),
+        head_diameter_(0),
+        tail_width_(0),
+        padding0__(0) {
+    (void)padding0__;
+  }
+  Settings(uint32_t _max_clients, float _map_width, float _map_height, float _speed, double _dt, uint32_t _max_length, float _head_diameter, float _tail_width)
+      : max_clients_(flatbuffers::EndianScalar(_max_clients)),
+        map_width_(flatbuffers::EndianScalar(_map_width)),
+        map_height_(flatbuffers::EndianScalar(_map_height)),
+        speed_(flatbuffers::EndianScalar(_speed)),
+        dt_(flatbuffers::EndianScalar(_dt)),
+        max_length_(flatbuffers::EndianScalar(_max_length)),
+        head_diameter_(flatbuffers::EndianScalar(_head_diameter)),
+        tail_width_(flatbuffers::EndianScalar(_tail_width)),
+        padding0__(0) {
+    (void)padding0__;
+  }
   uint32_t max_clients() const {
-    return GetField<uint32_t>(VT_MAX_CLIENTS, 0);
+    return flatbuffers::EndianScalar(max_clients_);
   }
   float map_width() const {
-    return GetField<float>(VT_MAP_WIDTH, 0.0f);
+    return flatbuffers::EndianScalar(map_width_);
   }
   float map_height() const {
-    return GetField<float>(VT_MAP_HEIGHT, 0.0f);
+    return flatbuffers::EndianScalar(map_height_);
   }
   float speed() const {
-    return GetField<float>(VT_SPEED, 0.0f);
+    return flatbuffers::EndianScalar(speed_);
   }
   double dt() const {
-    return GetField<double>(VT_DT, 0.0);
+    return flatbuffers::EndianScalar(dt_);
   }
   uint32_t max_length() const {
-    return GetField<uint32_t>(VT_MAX_LENGTH, 0);
+    return flatbuffers::EndianScalar(max_length_);
   }
   float head_diameter() const {
-    return GetField<float>(VT_HEAD_DIAMETER, 0.0f);
+    return flatbuffers::EndianScalar(head_diameter_);
   }
   float tail_width() const {
-    return GetField<float>(VT_TAIL_WIDTH, 0.0f);
+    return flatbuffers::EndianScalar(tail_width_);
+  }
+};
+FLATBUFFERS_STRUCT_END(Settings, 40);
+
+struct Reply FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
+  typedef ReplyBuilder Builder;
+  enum FlatBuffersVTableOffset FLATBUFFERS_VTABLE_UNDERLYING_TYPE {
+    VT_PLAYER_ID = 4,
+    VT_SETTINGS = 6
+  };
+  uint32_t player_id() const {
+    return GetField<uint32_t>(VT_PLAYER_ID, 0);
+  }
+  const dctl::flat_settings::Settings *settings() const {
+    return GetStruct<const dctl::flat_settings::Settings *>(VT_SETTINGS);
   }
   bool Verify(flatbuffers::Verifier &verifier) const {
     return VerifyTableStart(verifier) &&
-           VerifyField<uint32_t>(verifier, VT_MAX_CLIENTS) &&
-           VerifyField<float>(verifier, VT_MAP_WIDTH) &&
-           VerifyField<float>(verifier, VT_MAP_HEIGHT) &&
-           VerifyField<float>(verifier, VT_SPEED) &&
-           VerifyField<double>(verifier, VT_DT) &&
-           VerifyField<uint32_t>(verifier, VT_MAX_LENGTH) &&
-           VerifyField<float>(verifier, VT_HEAD_DIAMETER) &&
-           VerifyField<float>(verifier, VT_TAIL_WIDTH) &&
+           VerifyField<uint32_t>(verifier, VT_PLAYER_ID) &&
+           VerifyField<dctl::flat_settings::Settings>(verifier, VT_SETTINGS) &&
            verifier.EndTable();
   }
 };
 
-struct SettingsBuilder {
-  typedef Settings Table;
+struct ReplyBuilder {
+  typedef Reply Table;
   flatbuffers::FlatBufferBuilder &fbb_;
   flatbuffers::uoffset_t start_;
-  void add_max_clients(uint32_t max_clients) {
-    fbb_.AddElement<uint32_t>(Settings::VT_MAX_CLIENTS, max_clients, 0);
+  void add_player_id(uint32_t player_id) {
+    fbb_.AddElement<uint32_t>(Reply::VT_PLAYER_ID, player_id, 0);
   }
-  void add_map_width(float map_width) {
-    fbb_.AddElement<float>(Settings::VT_MAP_WIDTH, map_width, 0.0f);
+  void add_settings(const dctl::flat_settings::Settings *settings) {
+    fbb_.AddStruct(Reply::VT_SETTINGS, settings);
   }
-  void add_map_height(float map_height) {
-    fbb_.AddElement<float>(Settings::VT_MAP_HEIGHT, map_height, 0.0f);
-  }
-  void add_speed(float speed) {
-    fbb_.AddElement<float>(Settings::VT_SPEED, speed, 0.0f);
-  }
-  void add_dt(double dt) {
-    fbb_.AddElement<double>(Settings::VT_DT, dt, 0.0);
-  }
-  void add_max_length(uint32_t max_length) {
-    fbb_.AddElement<uint32_t>(Settings::VT_MAX_LENGTH, max_length, 0);
-  }
-  void add_head_diameter(float head_diameter) {
-    fbb_.AddElement<float>(Settings::VT_HEAD_DIAMETER, head_diameter, 0.0f);
-  }
-  void add_tail_width(float tail_width) {
-    fbb_.AddElement<float>(Settings::VT_TAIL_WIDTH, tail_width, 0.0f);
-  }
-  explicit SettingsBuilder(flatbuffers::FlatBufferBuilder &_fbb)
+  explicit ReplyBuilder(flatbuffers::FlatBufferBuilder &_fbb)
         : fbb_(_fbb) {
     start_ = fbb_.StartTable();
   }
-  flatbuffers::Offset<Settings> Finish() {
+  flatbuffers::Offset<Reply> Finish() {
     const auto end = fbb_.EndTable(start_);
-    auto o = flatbuffers::Offset<Settings>(end);
+    auto o = flatbuffers::Offset<Reply>(end);
     return o;
   }
 };
 
-inline flatbuffers::Offset<Settings> CreateSettings(
+inline flatbuffers::Offset<Reply> CreateReply(
     flatbuffers::FlatBufferBuilder &_fbb,
-    uint32_t max_clients = 0,
-    float map_width = 0.0f,
-    float map_height = 0.0f,
-    float speed = 0.0f,
-    double dt = 0.0,
-    uint32_t max_length = 0,
-    float head_diameter = 0.0f,
-    float tail_width = 0.0f) {
-  SettingsBuilder builder_(_fbb);
-  builder_.add_dt(dt);
-  builder_.add_tail_width(tail_width);
-  builder_.add_head_diameter(head_diameter);
-  builder_.add_max_length(max_length);
-  builder_.add_speed(speed);
-  builder_.add_map_height(map_height);
-  builder_.add_map_width(map_width);
-  builder_.add_max_clients(max_clients);
+    uint32_t player_id = 0,
+    const dctl::flat_settings::Settings *settings = 0) {
+  ReplyBuilder builder_(_fbb);
+  builder_.add_settings(settings);
+  builder_.add_player_id(player_id);
   return builder_.Finish();
 }
 
-inline const dctl::flat_settings::Settings *GetSettings(const void *buf) {
-  return flatbuffers::GetRoot<dctl::flat_settings::Settings>(buf);
+inline const dctl::flat_settings::Reply *GetReply(const void *buf) {
+  return flatbuffers::GetRoot<dctl::flat_settings::Reply>(buf);
 }
 
-inline const dctl::flat_settings::Settings *GetSizePrefixedSettings(const void *buf) {
-  return flatbuffers::GetSizePrefixedRoot<dctl::flat_settings::Settings>(buf);
+inline const dctl::flat_settings::Reply *GetSizePrefixedReply(const void *buf) {
+  return flatbuffers::GetSizePrefixedRoot<dctl::flat_settings::Reply>(buf);
 }
 
-inline const char *SettingsIdentifier() {
-  return "SETT";
+inline const char *ReplyIdentifier() {
+  return "REP_";
 }
 
-inline bool SettingsBufferHasIdentifier(const void *buf) {
+inline bool ReplyBufferHasIdentifier(const void *buf) {
   return flatbuffers::BufferHasIdentifier(
-      buf, SettingsIdentifier());
+      buf, ReplyIdentifier());
 }
 
-inline bool VerifySettingsBuffer(
+inline bool VerifyReplyBuffer(
     flatbuffers::Verifier &verifier) {
-  return verifier.VerifyBuffer<dctl::flat_settings::Settings>(SettingsIdentifier());
+  return verifier.VerifyBuffer<dctl::flat_settings::Reply>(ReplyIdentifier());
 }
 
-inline bool VerifySizePrefixedSettingsBuffer(
+inline bool VerifySizePrefixedReplyBuffer(
     flatbuffers::Verifier &verifier) {
-  return verifier.VerifySizePrefixedBuffer<dctl::flat_settings::Settings>(SettingsIdentifier());
+  return verifier.VerifySizePrefixedBuffer<dctl::flat_settings::Reply>(ReplyIdentifier());
 }
 
-inline const char *SettingsExtension() {
-  return "sett";
+inline const char *ReplyExtension() {
+  return "reply";
 }
 
-inline void FinishSettingsBuffer(
+inline void FinishReplyBuffer(
     flatbuffers::FlatBufferBuilder &fbb,
-    flatbuffers::Offset<dctl::flat_settings::Settings> root) {
-  fbb.Finish(root, SettingsIdentifier());
+    flatbuffers::Offset<dctl::flat_settings::Reply> root) {
+  fbb.Finish(root, ReplyIdentifier());
 }
 
-inline void FinishSizePrefixedSettingsBuffer(
+inline void FinishSizePrefixedReplyBuffer(
     flatbuffers::FlatBufferBuilder &fbb,
-    flatbuffers::Offset<dctl::flat_settings::Settings> root) {
-  fbb.FinishSizePrefixed(root, SettingsIdentifier());
+    flatbuffers::Offset<dctl::flat_settings::Reply> root) {
+  fbb.FinishSizePrefixed(root, ReplyIdentifier());
 }
 
 }  // namespace flat_settings
